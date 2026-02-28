6440eed169b9daa31e2c3768ff31f212d0e28985
AryanM0NKE <officialaryank21@gmail.com>
2026-02-28 14:42:25 +0530

    changed the app.jsx file again to run only the QFX model

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
-import React, { useState } from "react"
-import PDF3DViewer from "./components/PDF3DViewer"
+import React from "react"
 import QFXKioskViewer from "./components/QFXKioskViewer"
 
 export default function App() {
-  const [view, setView] = useState("pdf")
-
-  return (
-    <>
-      {/* UI buttons */}
-      <div style={{
-        position: "absolute",
-        top: "20px",
-        right: "20px",
-        zIndex: 100
-      }}>
-        <button onClick={() => setView("pdf")}>
-          Floorplan Viewer
-        </button>
-
-        <button onClick={() => setView("kiosk")}>
-          Kiosk Viewer
-        </button>
-      </div>
-
-      {/* Switch between models */}
-      {view === "pdf" ? <PDF3DViewer /> : <QFXKioskViewer />}
-    </>
-  )
+  return <QFXKioskViewer />
 }
